8ad858dc6ed7ddb38b45f1859c75f743284438a4
Muhammad Atif khan <22pwbcs0906@uetpeshawar.edu.pk>
2026-01-22 18:43:51 +0500

    Level 1 and 2  is done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,9 @@
 import Image from "next/image";
 
-export default function Home() {
+export default async function Home() {
+
+ await new Promise((resolve)=>setTimeout(resolve ,3000)) 
+
   return (
    <div className="text-black" >
     Hello From Home
